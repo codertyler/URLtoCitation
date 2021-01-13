3919833c3f5b1b3b59f806e0a3366ff4a390f8be
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-01-13 02:52:07 -0700

    trying to fix the error

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post("/api/URLs", (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
 });
 const port = process.env.PORT || 5000;
 app.listen(port);
